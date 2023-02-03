0a24c3a9c13b9f32d0b1393013e02eddac728195
채란 <nemo20435@gmail.com>
2023-02-03 09:56:22 +0900

    검색했을때 알림창 띄움

--- kdt-emart-app/assets/components/MainSection.js
+++ kdt-emart-app/assets/components/MainSection.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Modal, View, Pressable, Text } from "react-native";
 import styled from "styled-components/native";
 import EmartInputUi from "./EmartInputUi";
 
@@ -10,27 +11,57 @@ const MainScrollWrapper = styled.ScrollView`
   background-color: #f1f1f1;
 `;
 
+const ModalView = styled.View`
+  margin: auto;
+  width:80%;
+  height: 300px;
+  background-color: #222;
+  border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+`;
+
 const MainSection = () => {
 
-  const[data, setData] = useState();
+  const [data, setData] = useState();
+  const [modalIsView, setModalIsView] = useState(false);
 
-  const getData = async (searchText) => {
+  const getData = async (searchText) => { 
+    setData(searchText);
+    setModalIsView(true);
     console.log("Search Data", searchText)
-    //setData(data)
   }
 
   useEffect( ()=> {
     console.log("I am MainSection components", data);
   },[data])
 
+
   return (
+    <>
     <MainScrollWrapper>
-      {/* EmartInputUi에 적힌 placeholder가 출력되는데, 검색창을 여러개 만들어서 응용할 수 있다. */}
+      <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalIsView}
+          onRequestClose={() => {
+            setModalIsView(false);
+          }}
+      >
+        <ModalView>
+          <Pressable onPress={()=> setModalIsView(false)}>
+            <Text style={{color: '#fff'}}>Close</Text>
+          </Pressable>
+        </ModalView>
+      </Modal>
+
+      {/* EmartInputUi에 적힌 placeholder가 출력되는데, 검색창을 여러개 만들어서 응용할 수 있다. */}     
       <EmartInputUi 
         placeholder="검색어 입력!"
         getData={getData}
       />
     </MainScrollWrapper>
+    </>
   );
 }
 
